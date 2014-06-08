66b33d08e9c347bcfa62ee11495905ed7184656c
Akshar Raaj <akshar@agiliq.com>
2014-06-08 20:35:43 +0530

    Make registration view as CBV

--- registration/views.py
+++ registration/views.py
@@ -1,42 +1,40 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import FormView
 
 from .forms import RegistrationForm
 from .signals import registered
 
 
-def register(request, form_class=RegistrationForm,
-             template_name='registration/register.html',
-             redirect_url='registration_register_done', **kwargs):
-    form = form_class()
-    if request.method == 'POST':
-        form = form_class(request.POST)
-        if form.is_valid():
-            process_registration_form(request, form)
-            registered.send(sender=User, request=request, form=form)
-            return HttpResponseRedirect(reverse(redirect_url))
-    data = {'registration_form': form}
-    data.update(kwargs)
-    return render(request, template_name, data)
+class RegisterView(FormView):
+    template_name = 'registration/register.html'
+    form_class = RegistrationForm
+    success_url = reverse_lazy('registration_register_done')
 
-def process_registration_form(request, form):
-    """
-    Expects following arguments:
-    request: HttpRequest instance
-    Valid POSTed form
-    Creates a User instance with POSTed form
-    """
-    if 'username' not in form.cleaned_data:
-        form.cleaned_data['username'] = ''
-    user_data = {}
-    user_fields = [f.name for f in User._meta.fields]
-    for field, value in form.cleaned_data.items():
-        if field in user_fields:
-            user_data[field] = value
-    user = User.objects.create_user(**user_data)
-    return user
+    def form_valid(self, form):
+        # A valid form is POSTed
+        self.process_registration_form(self.request, form)
+        registered.send(sender=User, request=self.request, form=form)
+        return super(RegisterView, self).form_valid(form)
+
+    def process_registration_form(self, request, form):
+        """
+        Expects following arguments:
+        request: HttpRequest instance
+        Valid POSTed form
+        Creates a User instance with POSTed form
+        """
+        user_data = {}
+        user_fields = [f.name for f in User._meta.fields]
+        for field, value in form.cleaned_data.items():
+            if field in user_fields:
+                user_data[field] = value
+        user = User.objects.create_user(**user_data)
+        return user
+
+register = RegisterView.as_view()
+
 
 def register_done(request, template_name='registration/register_done.html',
                   **kwargs):
